f387349639571d8d9af42f7d834bdbffb3824053
bdsmith147 <31423769+bdsmith147@users.noreply.github.com>
2021-03-05 21:34:21 -0700

    Update GravarSe.py
    Cleans code and adds comments.

--- GravarSe.py
+++ GravarSe.py
@@ -12,59 +12,58 @@ import pandas as pd
 import datetime as dt
 from time import time as t
 
+#Uncomment each block block below one at a time to extract data from each cemetery.
+
+#Cemetery 1:
 #cemetery_name = 'sorsele-skogskyrkogard'
 #blocks = np.array(['1', '2', '3', '4', '6', '7', '8', 'AL', '10', '11', '12', '13', '13ML', '14', '15'])
 #pages_in_block = np.array([4, 2, 6, 4, 7, 6, 3, 1, 3, 1, 1, 3, 1, 2, 1])
 
+#Cemetery 2:
 #cemetery_name = 'sorsele-gamla-kyrkogard'
 #blocks = np.array(['0'])
 #pages_in_block = np.array([21])
 
+#Cemetery 3:
 #cemetery_name = 'skogskyrkogarden'
 #blocks = np.array(['1', '2', '3', '4', '5', '7', '8', 'a', 'allm', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
 #pages_in_block = np.array([2, 3, 3, 5, 5, 3, 2, 8, 3, 4, 4, 10, 5, 5, 6, 6])
 
+#Cemetery 4:
 #cemetery_name = 'viktoriakyrkans-kyrkogard'
 #blocks = np.array(['0'])
 #pages_in_block = np.array([1])
 
+#Cemetery 5:
 #cemetery_name = 'mala-gamla'
 #blocks = np.array(['askf', 'aslu', 'maga', 'milu', 'urnf'])
 #pages_in_block = np.array([1, 0, 11, 2, 1])
 
-cemetery_name = 'gargnas-kyrkogard'
-blocks = np.array(['1', '2', '3', '3ml'])
-pages_in_block = np.array([18, 6, 2, 1])
+#Cemetery 6:
+#cemetery_name = 'gargnas-kyrkogard'
+#blocks = np.array(['1', '2', '3', '3ml'])
+#pages_in_block = np.array([18, 6, 2, 1])
 
 
-#index = 14
-#blocks = np.array([blocks[index]])
-#pages_in_block = np.array([pages_in_block[index]])
 numBlocks = len(blocks)
-#print(blocks)
 
 mainURL = 'http://gravar.se/en/forsamling/mala-sorsele-pastorat/' + cemetery_name + '/0/'
-#block_links = np.array([])
-#block_names = np.array([])
-#block_births = np.array([])
-#block_deaths = np.array([])
-#block_sites = np.array([])
-#block_datesnums = np.array([])
-#%%
+
 entries = []
 AllData = pd.DataFrame(columns=['Block', 'Names', 'Births', 'Deaths', 'Site', 'Link'])
 for b in range(numBlocks):
     blockURL = mainURL + blocks[b] + '/page/'
     domain = 'http://gravar.se'
-    block_labels = np.array([])
-    block_links = np.array([])
-    block_names = np.array([])
-    block_births = np.array([])
-    block_deaths = np.array([])
-    block_sites = np.array([])
-    block_datesnums = np.array([])
     
-    for i in range(pages_in_block[b]):
+    # Generate arrays to append data to
+    block_labels = np.array([]) # The cemetery block designator
+    block_links = np.array([]) # All URLs to individual pages
+    block_names = np.array([]) # All full names
+    block_births = np.array([]) # All birth dates
+    block_deaths = np.array([]) # All death dates
+    block_sites = np.array([]) # The grave sites within the cemetery block
+    
+    for i in range(pages_in_block[b]): # Extract the data from each page in a cemetery block
         pnum = str(i+1)
         page = requests.get(blockURL + pnum)
         tree = html.fromstring(page.content)
@@ -93,7 +92,6 @@ for b in range(numBlocks):
     block_blocks = np.array([blocks[b] * len(block_names)])
     print(blocks[b], ': ', len(block_names))
     entries.append(len(block_names))
-    #print(block_births)
 
     data = np.stack([block_labels, block_names, block_births, block_deaths, block_sites, block_links]).T
     df = pd.DataFrame(data, columns=['Block', 'Names', 'Births', 'Deaths', 'Site', 'Link'])
